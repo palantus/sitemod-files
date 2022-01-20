cad71cb7c5777603c4479994ec6f8b8dad6d44c5
Anders Houbak Kristiansen <palantus@gmail.com>
2022-01-20 11:54:27 +0100

    Permissions

--- services.mjs
+++ services.mjs
@@ -2,15 +2,9 @@ import Permission from "../../models/permission.mjs"
 import Role from "../../models/role.mjs"
 
 export default async () => {
-  
-  Permission.lookupOrCreate("file.upload")
-  Permission.lookupOrCreate("file.edit")
-  Permission.lookupOrCreate("file.read")
-  Permission.lookupOrCreate("file.source.manage")
-
   // init
-  Role.lookupOrCreate("files").addPermission(["file.upload", "file.edit", "file.read"])
-  Role.lookupOrCreate("admin").addPermission(["file.source.manage"])
+  Role.lookupOrCreate("files").addPermission(["file.upload", "file.edit", "file.read"], true)
+  Role.lookupOrCreate("admin").addPermission(["file.source.manage"], true)
 
   return {
   }
