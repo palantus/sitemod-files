2f4b5d7295f658969a31e9596e6f12fe9683395f
Anders Houbak Kristiansen <palantus@gmail.com>
2022-03-03 12:39:53 +0100

    Fix

--- services.mjs
+++ services.mjs
@@ -8,7 +8,7 @@ export default async () => {
   Role.lookupOrCreate("files").addPermission(["file.upload", "file.edit", "file.read"], true)
   Role.lookupOrCreate("admin").addPermission(["file.source.manage"], true)
 
-  let folderType = DataType.lookupOrCreate("folder", {title: "Folder", permission: "file.read", api: "file", nameField: "name", uiPath: "folder", query: "tag:folder", acl: "r:inherit;w:inherit", aclInheritance: true})
+  let folderType = DataType.lookupOrCreate("folder", {title: "Folder", permission: "file.read", api: "file", nameField: "name", uiPath: "folder", acl: "r:inherit;w:inherit", aclInheritance: true})
                            .init({typeModel: Folder})
   DataType.lookupOrCreate("file", {title: "File", permission: "file.read", api: "file", nameField: "name", uiPath: "file", acl: "r:inherit;w:inherit", aclInheritance: true, aclInheritFrom: folderType})
           .init({typeModel: File})
