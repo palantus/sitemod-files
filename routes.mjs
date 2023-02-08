7a4f37d53fbe1e375a096cfe8c590eabe04a5473
Anders Houbak Kristiansen <palantus@gmail.com>
2023-02-08 16:59:45 +0100

    Add download page for federation

--- routes.mjs
+++ routes.mjs
@@ -1,5 +1,6 @@
 routes.push(...[
   {path: "/drop",                   page: "/pages/drop.mjs"},
+  {regexp: /^\/file\/download\/([a-zA-Z\d]+)/,page: "/pages/download.mjs", publicAccess: true},
   {regexp: /^\/file\/([a-zA-Z\d]+)/,page: "/pages/file.mjs", publicAccess: true},
   {regexp: /^\/folder\/(\d+)/,      page: "/pages/files.mjs", publicAccess: true},
   {regexp: /^\/files-search/,       page: "/pages/files-search.mjs", publicAccess: true},
---
+++ www/pages/download.mjs
@@ -0,0 +1,99 @@
+const elementName = 'file-download-page'
+
+import {state, setPageTitle, apiURL} from "/system/core.mjs"
+import api from "/system/api.mjs"
+import "/components/field-ref.mjs"
+import { alertDialog } from "../../components/dialog.mjs"
+import {sizeToName} from "./file.mjs"
+
+const template = document.createElement('template');
+template.innerHTML = `
+
+  <link rel='stylesheet' href='/css/global.css'>
+  <link rel='stylesheet' href='/css/searchresults.css'>
+  <style>
+    #container{
+      padding: 10px;
+    }
+    .hidden{display: none;}
+    #subtitle{color: gray; margin-left: 10px;}
+    #maintitle {margin-bottom: 0px;}
+    #subtitle-size, #subtitle-owner, #subtitle-modified{color: var(--accent-color-light);}
+  </style>
+
+  <div id="container">
+    <h2 id="maintitle"><span id="title"></span></h2>
+    <div id="subtitle">
+      (<span id="subtitle-type"></span> owned by <span id="subtitle-owner"></span><span id="subtitle-size-container"> of size <span id="subtitle-size"></span></span>, last edited <span id="subtitle-modified"></span>)
+    </div>
+    <br>
+    <button id="download-btn" class="styled">Download now</button>
+    </div>
+`;
+
+class Element extends HTMLElement {
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+    this.downloadFile = this.downloadFile.bind(this)
+
+    this.shadowRoot.getElementById("download-btn").addEventListener("click", this.downloadFile)
+
+    this.fileId = /([\da-zA-Z]+)/.exec(state().path.split("/")[3])[0]
+  }
+
+  async refreshData(id = this.fileId){
+    setPageTitle("");
+    let src = this.federationSource = state().query.src
+    let file = this.file = await api.get(src ? `federation/${src}/api/file/${this.fileId}` : `file/${this.fileId}`)
+    
+    if(!file){
+      alertDialog("This file or folder could not be found. Either it doesn't exist or you do not have access to it")
+      return;
+    }
+
+    this.fileId = file.id
+    setPageTitle(file.name);
+
+    if(file.type == "file"){
+      let sizeName = sizeToName(file.size)
+
+      this.shadowRoot.getElementById('subtitle-size').innerText = sizeName
+    }
+
+    this.shadowRoot.getElementById('subtitle-type').innerText = file.type == "folder" ? "Folder" : "File"
+    this.shadowRoot.getElementById('subtitle-owner').innerText = file.ownerId
+    this.shadowRoot.getElementById('subtitle-modified').innerText = (file.modified||file.created||"").replace("T", " ").substring(0, 19)
+    this.shadowRoot.getElementById('subtitle-size-container').classList.toggle("hidden", file.type != "file")
+    
+    this.shadowRoot.getElementById('title').innerText = file.name
+
+    this.shadowRoot.getElementById('download-btn').classList.toggle("hidden", file.type != "file")
+
+    this.lastFileId = this.fileId
+  }
+
+  async downloadFile(){
+    if(this.federationSource){
+      let {token} = await api.get("me/token")
+      window.open(`${apiURL()}/federation/${this.federationSource}/api/file/download/${this.fileId}?token=${token}`)
+      console.log(`${apiURL()}/federation/${this.federationSource}/api/file/download/${this.fileId}?token=${token}`)
+    } else {
+      window.open(this.file.links?.download) 
+    }
+  }
+
+  connectedCallback() {
+    this.refreshData(this.fileId);
+  }
+
+  disconnectedCallback() {
+  }
+
+}
+
+window.customElements.define(elementName, Element);
+export {Element, elementName as name}
