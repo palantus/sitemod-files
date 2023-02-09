15abe6a1becc3078e5cc92dae9e527e2d556a2c5
Anders Houbak Kristiansen <palantus@gmail.com>
2023-02-09 12:07:05 +0100

    Allow edit javascript files

--- www/load-mod-files.mjs
+++ www/load-mod-files.mjs
@@ -2,4 +2,5 @@ import {addFileAction} from "/libs/actions.mjs"
 
 export async function load(){
   addFileAction("text/plain", {title: "Edit", componentName: "file-edit-text-component", access: "w", permission: "file.edit"})
+  addFileAction("application/javascript", {title: "Edit", componentName: "file-edit-text-component", access: "w", permission: "file.edit"})
 }
