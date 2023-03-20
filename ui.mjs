d7f35aee3142e2601eaaa14b5d1aa2d158c332f8
Anders Houbak Kristiansen <palantus@gmail.com>
2023-03-20 13:14:01 +0100

    Menu change

--- ui.mjs
+++ ui.mjs
@@ -2,8 +2,14 @@ export let menu = [
   {
     title: "Files",
     items: [
+      {title: "Files", path: "/files/mine", permission: "file.read"}
+    ]
+  },
+  {
+    title: "Files",
+    items: [
       {title: "Drop", path: "/drop"},
-      {title: "Files", path: "/files", permission: "file.read"}
+      {title: "Shared files", path: "/files/shared", permission: "file.read"}
     ]
   }
 ]
